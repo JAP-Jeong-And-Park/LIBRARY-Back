eecd1d0938185b46c8749b0d560042ab03aa69f9
정우재 <80890472+woojae05@users.noreply.github.com>
2022-04-01 23:43:13 +0900

    init

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const app = express();
-const router = require("./router/index");
+const router = require("./src/routers/index");
 
 app.use(router);
-// app.get("/", (req, res) => {
-//   res.send("hello node");
-// });
-
-// app.listen(4000, () => console.log("4000번 포트에서 대기중"));
--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,9 @@ const app = express();
 const mysql = require("mysql");
 var router = express.Router();
 var main = require("./main/main");
+var register = require("./register/register");
 
 router.use("/", main);
+router.use("/register", register);
 
 module.exports = router;
